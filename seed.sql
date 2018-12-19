481d1b86fdb6d1440ce7a5507a0102b866e61db6
Michell Tejada <michelltejada@pursuit.com>
2018-12-19 09:08:35 -0500

    Added bonus

--- seed.sql
+++ seed.sql
@@ -45,3 +45,22 @@ CREATE TABLE pictures(
   album_id INT REFERRENCES albums(id) ON DELETE CASCADE,
   url VARCHAR NOT NULL
 );
+
+
+INSERT INTO users(name, age)
+VALUES('Michell', 25), ('James', 12), ('Tommy', 17)
+
+INSERT INTO posts(poster_id, body)
+VALUES(1, 'Welcome'), (2, 'To this'), (3, 'Cool thing')
+
+INSERT INTO likes(liker_id, post_id)
+VALUES(2, 1), (3, 2), (1, 3)
+
+INSERT INTO comments(commenter_id, post_id, body)
+VALUES(3, 1, 'A comment'), (3, 2, 'Another comment'), (1, 3, 'Wow ANOTHER comment?')
+
+INSERT INTO albums(user_id)
+VALUES(1), (2), (3)
+
+INSERT INTO pictures(album_id, url)
+VALUES(1, 'www.picture1.com'), (2, 'www.picture2.com'), (3, 'www.picture3.com')
